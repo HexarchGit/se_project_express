b593084559084c3c239d008c6b11509483e8ab28
Sergei Sushko <sushkosa89@gmail.com>
2025-06-09 16:20:48 +0300

    Moved temporaty autorization into app.js

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const app = express();
 const mongoose = require("mongoose");
 const userRouter = require("./routes/users");
 const itemRouter = require("./routes/clothingItems");
-const authorization = require("./middlewares/authorization");
+// const authorization = require("./middlewares/authorization");
 const { NotFoundError } = require("./utils/errors");
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
-app.use(authorization);
+app.use(
+  (module.exports = (req, _res, next) => {
+    req.user = { _id: "6844a627c9d7f248f2ad5057" };
+    next();
+  })
+);
 app.use("/users", userRouter);
 app.use("/items", itemRouter);
 app.use((_req, _res, next) => {
