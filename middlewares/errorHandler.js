227379b5fb8363b36a26a59c678d18bbbaef65b1
Sergei Sushko <sushkosa89@gmail.com>
2025-06-10 17:04:07 +0300

    Called instance of the error class instead of class himself

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,11 @@
 const { AppError, ServerError } = require("../utils/errors");
+const serverError = new ServerError();
 
 module.exports = (error, _req, res, _next) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).send({ message: error.message });
   }
   return res
-    .status(ServerError.statusCode)
-    .send({ message: ServerError.message });
+    .status(serverError.statusCode)
+    .send({ message: serverError.message });
 };
